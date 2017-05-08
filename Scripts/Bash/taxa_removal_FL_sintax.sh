3c63fc47cc86c7283d65ea810dc99fd90793dc5a
AmeLaporte <amelie.lpe@sfr.fr>
2017-05-08 10:55:06 +0200

    Update taxa_removal_FL_sintax.sh

--- Scripts/Bash/taxa_removal_FL_sintax.sh
+++ Scripts/Bash/taxa_removal_FL_sintax.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-#for file in *;do
-#    echo ${file}
-#    python /media/amelie/DATA/Anders_TFM/Script/Python/cpx_sintax.py  /media/amelie/DATA/Anders_TFM/Sintax\ DB/unite.fasta /media/amelie/DATA/Anders_TFM/LCA\ DB/unite.fasta ${file}
-#done
+for file in *;do
+    echo ${file}
+    python /media/amelie/DATA/Anders_TFM/Script/Python/cpx_sintax.py  /media/amelie/DATA/Anders_TFM/Sintax\ DB/unite.fasta /media/amelie/DATA/Anders_TFM/LCA\ DB/unite.fasta ${file}
+done
 
 
 for file in *_db.fasta ;do
@@ -18,10 +18,10 @@ for file in *_db.fasta; do
     
     usearch -sintax ${taxon}.fasta -db UDB/${taxon}_db.udb -tabbedout ${taxon}.sintax -strand both -sintax_cutoff 0.8
 
-    #cat ${taxon}.sintax >> evaluate_all.sintax
+    cat ${taxon}.sintax >> evaluate_all.sintax
 done
 
-#python /media/amelie/DATA/Anders_TFM/Script/Python/evaluate_sintax_bis.py evaluate_all.sintax 0.1 result_evaluate.txt
+python /media/amelie/DATA/Anders_TFM/Script/Python/evaluate_sintax_bis.py evaluate_all.sintax 0.1 result_evaluate.txt
 
 
 mkdir SINTAX
@@ -32,11 +32,3 @@ mv *fasta FASTA/
 
 mkdir EVALUATE
 mv *.txt EVALUATE/
-
-
-
-
-#TODO: concatenate all the results so we will have only one output for evaluate!
-
-
-
